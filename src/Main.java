0667a0e0b30c82f41b663c25267b304f1c42e0f1
Vlatko Banović <vlatko.bane@gmail.com>
2024-10-13 20:47:37 +0200

    Prva zadaća

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        // prvi
+        System.out.println("Unesite prvi broj:");
+        int prviBroj = scanner.nextInt();
+        // drugi
+        System.out.println("Unesite drugi broj:");
+        int drugiBroj = scanner.nextInt();
+        // rezultat
+        System.out.println("Zbroj brojeva je: " + (prviBroj + drugiBroj));
+        System.out.println("Razlika brojeva je: " + (prviBroj - drugiBroj));
+        System.out.println("Umnožak brojeva je: " + (prviBroj * drugiBroj));
+        System.out.println("Kvocijent brojeva je: " + ((float)prviBroj / drugiBroj));
     }
 }
