666508a10960821f84dc75d2020b19fde135f284
Vlatko Banović <vlatko.bane@gmail.com>
2024-11-05 22:00:16 +0100

    Zadaća 30.10.

--- src/Main.java
+++ src/Main.java
@@ -5,22 +5,14 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Unesite iznos koji je potrebno vratiti: ");
+        Polaznik polaznik1 = new Polaznik("Matej","Lelek");
+        Polaznik polaznik2 = new Polaznik("Marko","Markić",25, "M" );
+        Polaznik polaznik3 = new Polaznik();
 
-        double kusur = scanner.nextDouble();
-        double[] apoeni = {500, 200, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01};
+        polaznik2.promjeniDob(26);
+        polaznik3.promjeniIme("Luka");
+        polaznik3.promjeniPrezime("Lučić");
 
-        List<Double> rezultat = new ArrayList<>();
-        for (double apoen : apoeni) {
-            while (kusur >= apoen) {
-                rezultat.add(apoen);
-                kusur -= apoen;
-                kusur = Math.round(kusur * 100.0) / 100.0;
-            }
-        }
-
-        System.out.println("Kusur iznosi: " + rezultat);
-        scanner.close();
+        System.out.println(polaznik1.dohvatiIme() + " " + polaznik1.dohvatiPrezime());
     }
 }
