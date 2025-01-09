de25cfb219dd7f44150b5b320c66655ac0755aa3
Vlatko Banović <vlatko.bane@gmail.com>
2025-01-09 20:55:47 +0100

    Zadatak 09.01.2025.

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,95 @@
 import java.util.Scanner;
-import java.util.TreeSet;
+import java.util.HashMap;
+
 
 public class Main {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-            TreeSet<String> gradovi = new TreeSet<>();
-            Scanner scanner = new Scanner(System.in);
+        class Polaznik {
+            private String ime;
+            private String prezime;
+            private String email;gi
 
-            System.out.println("Unesite nazive gradova (za završetak unesite 'kraj'):");
-            while (true) {
-
-                String grad = scanner.nextLine();
-
-                if (grad.equalsIgnoreCase("kraj")) break;
-
-                gradovi.add(grad);
+            public Polaznik(String ime, String prezime, String email) {
+                this.ime = ime;
+                this.prezime = prezime;
+                this.email = email;
             }
 
-            System.out.println("Gradovi u sortiranom redoslijedu:");
-
-            for (String grad : gradovi) {
-
-                System.out.println(grad);
+            public String getIme() {
+                return ime;
             }
 
-            if (!gradovi.isEmpty()) {
-
-                System.out.println("Prvi grad: " + gradovi.first());
-
-                System.out.println("Zadnji grad: " + gradovi.last());
+            public String getPrezime() {
+                return prezime;
             }
 
-            System.out.println("Unesite naziv grada koji želite ukloniti:");
-
-            String gradZaBrisanje = scanner.nextLine();
-
-            if (gradovi.remove(gradZaBrisanje)) {
-
-                System.out.println("Grad '" + gradZaBrisanje + "' je uklonjen.");
-
-            } else {
-
-                System.out.println("Grad '" + gradZaBrisanje + "' nije pronađen.");
+            public String getEmail() {
+                return email;
             }
 
-            System.out.println("Preostali gradovi u sortiranom redoslijedu:");
-
-            for (String grad : gradovi) {
-
-                System.out.println(grad);
+            @Override
+            public String toString() {
+                return "Ime: " + ime + ", Prezime: " + prezime + ", Email: " + email;
             }
-            scanner.close();
         }
+
+        class EvidencijaPolaznika {
+            public static void main(String[] args) {
+                HashMap<String, Polaznik> evidencija = new HashMap<>();
+                Scanner scanner = new Scanner(System.in);
+
+                while (true) {
+                    System.out.println("Odaberite opciju:");
+                    System.out.println("1. Dodaj novog polaznika");
+                    System.out.println("2. Ispis svih polaznika");
+                    System.out.println("3. Izlaz");
+                    int opcija = scanner.nextInt();
+                    scanner.nextLine(); // Čišćenje buffera
+
+                    switch (opcija) {
+                        case 1:
+                            // Unos novog polaznika
+                            System.out.println("Unesite ime polaznika:");
+                            String ime = scanner.nextLine();
+                            System.out.println("Unesite prezime polaznika:");
+                            String prezime = scanner.nextLine();
+                            System.out.println("Unesite email polaznika:");
+                            String email = scanner.nextLine();
+
+                            // Provjera jedinstvenosti email adrese
+                            if (evidencija.containsKey(email)) {
+                                System.out.println("Polaznik s e-mail adresom '" + email + "' već postoji!");
+                            } else {
+                                Polaznik noviPolaznik = new Polaznik(ime, prezime, email);
+                                evidencija.put(email, noviPolaznik);
+                                System.out.println("Polaznik uspješno dodan.");
+                            }
+                            break;
+
+                        case 2:
+                            // Ispis svih polaznika
+                            if (evidencija.isEmpty()) {
+                                System.out.println("Evidencija je prazna.");
+                            } else {
+                                System.out.println("Lista svih polaznika:");
+                                for (Polaznik polaznik : evidencija.values()) {
+                                    System.out.println(polaznik);
+                                }
+                            }
+                            break;
+
+                        case 3:
+                            // Izlaz iz programa
+                            System.out.println("Izlaz iz programa.");
+                            scanner.close();
+                            return;
+
+                        default:
+                            System.out.println("Nepoznata opcija. Pokušajte ponovo.");
+                    }
+                }
+            }
+        }
+    }
 }
